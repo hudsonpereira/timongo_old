cea989b859ff66893ae1fed6b123010f80ae5ce1
Hudson Pereira <hudson.byte@gmail.com>
2018-09-13 11:11:26 -0300

    arsenal layout

--- resources/views/arsenal/index.blade.php
+++ resources/views/arsenal/index.blade.php
@@ -8,16 +8,41 @@
                 <div class="card-header">{{ __('Arsenal') }}</div>
 
                 <div class="card-body">
-                    <h1 class="display-5">Utilizando:</h1>
-                    @if($armor)
-                        <p><img src="images/{{$armor->image}}.gif" alt="" class="img-responsive"> {{ $armor->name }}</p>
-                    @endif
+                    <p class="text-justify">Aqui você encontra seu arsenal de equipamentos.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-sm-3">
+            <div class="card">
+                <div class="card-header">Equipados</div>
+                <div class="card-body">
+                    <div class="row justify-content-around">
+                        <div class="col-sm-3 arsenal"></div>
+                        <div class="col-sm-3 arsenal"></div>
+                        <div class="col-sm-3 arsenal"></div>
+                    </div>
 
-                    @if($weapon)
-                        <p><img src="images/leather-armor.gif" alt="" class="img-responsive"> Roupa de Couro</p>
-                    @endif
+                    <div class="row justify-content-around my-2 text-center">
+                        <div class="col-sm-3 arsenal"></div>
+                        <div class="col-sm-3 arsenal">
+                            @if($armor)
+                                <img src="images/{{$armor->image}}.gif" alt="" class="img-responsive py-3">
+                            @endif
+                            <!-- <img src="images/leather-armor.gif" alt="" class="img-responsive py-3"> -->
+                        </div>
+                        <div class="col-sm-3 arsenal">
+                            @if($weapon)
+                                <img src="images/leather-armor.gif" alt="" class="img-responsive py-3">
+                            @endif
+                            <!-- <img src="images/leather-armor.gif" alt="" class="img-responsive py-3"> -->
+                        </div>
+                    </div>
 
-                    <h1 class="display-5">Disponível:</h1>
+                    <div class="row justify-content-around">
+                        <div class="col-sm-3 arsenal"></div>
+                        <div class="col-sm-3 arsenal"></div>
+                        <div class="col-sm-3 arsenal"></div>
+                    </div>
                 </div>
             </div>
         </div>
